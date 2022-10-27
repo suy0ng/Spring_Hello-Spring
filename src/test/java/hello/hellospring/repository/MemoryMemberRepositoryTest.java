90a773be07e22c859e0a4fde77142e4d03806cd2
suy0ng <yunsuyong739@naver.com>
2022-10-27 15:05:58 +0900

    회원관리 리포지토리 생성

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+
+    }
+
+}
