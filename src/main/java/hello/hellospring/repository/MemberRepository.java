67318b7dabec1fac5be6cc959c802709d9ad5bbc
suy0ng <yunsuyong739@naver.com>
2022-10-30 15:22:47 +0900

    spring 스프링 빈과 의존관계 설정

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,9 +1,10 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
-
 public interface MemberRepository {
     Member save(Member member);
     Optional<Member> findById(long id);
